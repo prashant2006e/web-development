e5a174736b832e30eec8f055d28d4fcfb123edb0
prashant2006e <adds.prashant2006@gmail.com>
2026-06-30 18:53:21 +0530

    Fresh initial commit

---
+++ my-first-app/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App(){
+  //jscode
+  return (
+    //html code -Structure
+  <div>
+    
+    <h2>welcome to react</h2>
+    <p>this is my first react app</p>
+  <button>click me</button>
+  </div>
+  
+)
+}
+export default App;
---
+++ my-first-app/src/LOGINBUTTON.js
@@ -0,0 +1,10 @@
+import react from 'react';
+
+function Button(){
+  return(
+    <div>
+      <button>Login</button>
+    </div>
+  )
+}
+export default Button;
